6bbdafbcb66bcce0a190934866957d9bdbb7db25
Anadian <willanad@yandex.com>
2021-02-15 18:43:19 -0500

    chore: Preparing for first release.

---
+++ source/main.js
@@ -0,0 +1,178 @@
+#!/usr/local/bin/node
+'use strict';
+/**
+# [concise-buffer.js](source/concise-buffer.js)
+> Concisely serialise the beginning and end of a Node Buffer.
+
+Internal module name: `Concise-Buffer`
+
+Author: Anadian
+
+Code license: MIT
+```
+	Copyright 2020 Anadian
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+	The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
+Documentation License: [![Creative Commons License](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-sa/4.0/)
+> The source-code comments and documentation are written in [GitHub Flavored Markdown](https://github.github.com/gfm/).
+
+> The type notation used in this documentation is based off of the [Google Closure type system](https://github.com/google/closure-compiler/wiki/Types-in-the-Closure-Type-System).
+
+> The status and feature lifecycle keywords used in this documentation are based off of my own standard [defined here](https://github.com/Anadian/FeatureLifeCycleStateStandard).
+*/
+
+//#Dependencies
+	//##Internal
+	//##Standard
+	//##External
+//#Constants
+const FILENAME = 'concise-buffer.js';
+const MODULE_NAME = 'Concise-Buffer';
+var PACKAGE_JSON = {};
+var PROCESS_NAME = '';
+if(require.main === module){
+	PROCESS_NAME = 'concise-buffer';
+} else{
+	PROCESS_NAME = process.argv0;
+}
+//##Errors
+
+//#Global Variables
+/* istanbul ignore next */
+var Logger = { 
+	log: () => {
+		return null;
+	}
+};
+//#Functions
+/**
+## Functions
+*/
+/**
+### setLogger
+> Allows this module's functions to log the given logger object.
+
+Parametres:
+| name | type | description |
+| --- | --- | --- |
+| logger | {?object} | The logger to be used for logging or `null` to disable logging. |
+
+Throws:
+| code | type | condition |
+| --- | --- | --- |
+| 'ERR_INVALID_ARG_TYPE' | {TypeError} | Thrown if `logger` is neither an object nor `null` |
+
+History:
+| version | change |
+| --- | --- |
+| 0.0.0 | Introduced |
+*/
+function setLogger( logger ){
+	var return_error = null;
+	//const FUNCTION_NAME = 'setLogger';
+	//Variables
+	//Parametre checks
+	/* istanbul ignore else */
+	if( typeof(logger) === 'object' ){
+		/* istanbul ignore next */
+		if( logger === null ){
+			logger = { 
+				log: () => {
+					return null;
+				}
+			};
+		}
+	} else{
+		return_error = new TypeError('Param "logger" is not an object.');
+		return_error.code = 'ERR_INVALID_ARG_TYPE';
+		throw return_error;
+	}
+
+	//Function
+	Logger = logger;
+	//Return
+}
+/**
+### getStringFromBuffer
+> Returns a string with the first few and last few bytes of the buffer.
+
+Parametres:
+| name | type | description |
+| --- | --- | --- |
+| input_buffer | {Buffer} | The Buffer to generate a concise string representation for.  |
+| number_of_bytes | {number} | The number of bytes to show from the beginning and end of the buffer. \[default: \] |
+| options | {?Object} | [Reserved] Additional run-time options. \[default: {}\] |
+
+Returns:
+| type | description |
+| --- | --- |
+| {string} | Returns a string of the format Buffer:[ <first sixteen bytes in hexadecimal> ... <last sixteen bytes in hexadecimal> ]  |
+
+Throws:
+| code | type | condition |
+| --- | --- | --- |
+| 'ERR_INVALID_ARG_TYPE' | {TypeError} | Thrown if a given argument isn't of the correct type. |
+
+History:
+| version | change |
+| --- | --- |
+| 0.0.1 | WIP |
+*/
+function getStringFromBuffer( input_buffer, number_of_bytes = 16, options = {},){
+	var arguments_array = Array.from(arguments);
+	var _return;
+	var return_error;
+	const FUNCTION_NAME = 'getStringFromBuffer';
+	Logger.log({process: PROCESS_NAME, module: MODULE_NAME, file: FILENAME, function: FUNCTION_NAME, level: 'debug', message: `received: ${arguments_array}`});
+	//Variables
+	var beginning_string = '';
+	var ending_string = '';
+	//Parametre checks
+	if( Buffer.isBuffer(input_buffer) === false ){
+		return_error = new TypeError('Param "input_buffer" is not Buffer.');
+		return_error.code = 'ERR_INVALID_ARG_TYPE';
+		throw return_error;
+	}
+	if( typeof(number_of_bytes) !== 'number' ){
+		return_error = new TypeError('Param "number_of_bytes" is not number.');
+		return_error.code = 'ERR_INVALID_ARG_TYPE';
+		throw return_error;
+	}
+	if( typeof(options) !== 'object' ){
+		return_error = new TypeError('Param "options" is not ?Object.');
+		return_error.code = 'ERR_INVALID_ARG_TYPE';
+		throw return_error;
+	}
+
+	//Function
+	if( (2 * number_of_bytes) < input_buffer.length ){
+		beginning_string = input_buffer.toString( 'hex', 0, number_of_bytes );
+		ending_string = input_buffer.toString( 'hex', (input_buffer.length - number_of_bytes) );
+		_return = 'Buffer:[ '+beginning_string+' ... '+ending_string+' ]';
+	} else{
+		_return = 'Buffer:[ '+input_buffer.toString( 'hex' )+' ]';
+	}
+
+	//Return
+	Logger.log({process: PROCESS_NAME, module: MODULE_NAME, file: FILENAME, function: FUNCTION_NAME, level: 'debug', message: `returned: ${_return}`});
+	return _return;
+}
+//#Exports and Execution
+if(require.main === module){
+} else{
+	exports.setLogger = setLogger;
+	exports.getStringFromBuffer = getStringFromBuffer;
+}
---
+++ source/main.test.js
@@ -0,0 +1,93 @@
+#!/usr/local/bin/node
+'use strict';
+/**
+# [main.test.js](source/main.test.js)
+> The test file for `main.js`.
+
+Internal module name: `ConciseBufferTest`
+
+Author: Anadian
+
+Code license: MIT
+```
+	Copyright 2020 Anadian
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+	The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
+Documentation License: [![Creative Commons License](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-sa/4.0/)
+> The source-code comments and documentation are written in [GitHub Flavored Markdown](https://github.github.com/gfm/).
+
+> The type notation used in this documentation is based off of the [Google Closure type system](https://github.com/google/closure-compiler/wiki/Types-in-the-Closure-Type-System).
+
+> The status and feature lifecycle keywords used in this documentation are based off of my own standard [defined here](https://github.com/Anadian/FeatureLifeCycleStateStandard).
+*/
+
+//#Dependencies
+	//##Internal
+	const ConciseBuffer = require('./main.js');
+	//##Standard
+	const FileSystem = require('fs');
+	//##External
+	const AVA = require('ava');
+//#Constants
+//##Errors
+
+//#Global Variables
+/* istanbul ignore next */
+
+//#Functions
+AVA.before( function( t ){
+	const random_buffer_1024 = FileSystem.readFileSync( 'test/random_1024.dat' );
+	t.context.random_buffer_1024 = random_buffer_1024;
+} );
+AVA('getStringFromBuffer:Error:InvalidParam:input_buffer', function( t ){
+	t.throws( ConciseBuffer.getStringFromBuffer.bind( null ), {
+		instanceOf: TypeError,
+		code: 'ERR_INVALID_ARG_TYPE'
+	} );
+	t.pass();
+} );
+AVA('getStringFromBuffer:Error:InvalidParam:number_of_bytes', function( t ){
+	t.throws( ConciseBuffer.getStringFromBuffer.bind( t.context.random_buffer_1024, false ), {
+		instanceOf: TypeError,
+		code: 'ERR_INVALID_ARG_TYPE'
+	} );
+	t.pass();
+} );
+AVA('getStringFromBuffer:Error:InvalidParam:options', function( t ){
+	t.throws( ConciseBuffer.getStringFromBuffer.bind( t.context.random_buffer_1024, 8, false ), {
+		instanceOf: TypeError,
+		code: 'ERR_INVALID_ARG_TYPE'
+	} );
+	t.pass();
+} );
+AVA('getStringFromBuffer:Success:DefaultLength', function( t ){
+	t.is( ConciseBuffer.getStringFromBuffer( t.context.random_buffer_1024 ), 'Buffer:[ 8ae6d80ca5743723b87cf92945e24395 ... 8a9a710d39587d5d15b3756b35483ed8 ]' );
+} );
+AVA('getStringFromBuffer:Success:CustomLength', function( t ){
+	t.is( ConciseBuffer.getStringFromBuffer( t.context.random_buffer_1024, 8 ), 'Buffer:[ 8ae6d80ca5743723 ... 15b3756b35483ed8 ]' );
+} );
+AVA('getStringFromBuffer:Success:SmallerBufferLengthThanNumberOfBytes', function( t ){
+	const random_buffer_16 = t.context.random_buffer_1024.slice( 0, 16 );
+	t.is( ConciseBuffer.getStringFromBuffer( random_buffer_16, 32 ), 'Buffer:[ 8ae6d80ca5743723b87cf92945e24395 ]' );
+} );
+
+
+
+
+//#Exports and Execution
+if(require.main === module){
+} else{
+}
